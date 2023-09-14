f942c4da1be35a42ac28cf11b7f203073b3b2fa0
egosky <egoreg@naver.com>
2023-09-14 14:11:26 +0900

    gh-pages

--- pkg/event/event.go
+++
@@ -1,9 +0,0 @@
-package event
-
-const (
-	BackupError      string = "BakcupError"
-	BackupFailed     string = "BakcupFailed"
-	Starting         string = "Starting"
-	Successful       string = "Successful"
-	BackupSuccessful string = "SuccessfulBackup"
-)
--- pkg/utils/math.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "math"
-
-func Round(num float64) int {
-	return int(num + math.Copysign(0.5, num))
-}
